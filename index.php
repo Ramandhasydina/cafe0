d5b5130a37078015cefb0feeaae4807add1c3b6b
Ramandhasydina <63191634+Ramandhasydina@users.noreply.github.com>
2020-04-20 20:45:05 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<head>
+    <title>Cafe Coffee Shop</title>
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+
+</head>
+<body>
+<div id="art-page-background-middle-texture">
+	<div id="art-main">
+		<div class="cleared reset-box"></div>
+		<div class="art-header">
+			<div class="art-header-position">
+				<div class="art-header-wrapper">
+					<div class="cleared reset-box"></div>
+					<div class="art-header-inner">
+					 <div class="art-headerobject"></div>
+					
+					<div class="art-logo">
+									 <h1 class="art-logo-name">Coffee Shop SerbaBebas</h1>
+													 <h2 class="art-logo-text">Tempat Nongkrong Anak Muda Masa Kini</h2>
+									</div>
+									
+					</div>
+				</div>
+			</div>
+		  
+		</div>
+	   <div class="cleared reset-box"></div>
+		<div class="art-bar art-nav">
+		<div class="art-nav-outer">
+		<div class="art-nav-wrapper">
+		<div class="art-nav-inner">
+		<div class="art-nav-center">
+			<ul class="art-hmenu">
+				<li>
+					<a href="admin.php" class="active">Home</a>
+				</li>	
+				<li>
+					<a href="#">Menu Coffee Shop</a>
+					<ul>
+						<li><a href="admin.php?admin=daftarkategorimenu">Hamburger</a></li>
+						<li><a href="admin.php?admin=menudetil">Sandwich</a><li>
+						<li><a href="admin.php?admin=menudetil">Coffee</a><li>
+						<li><a href="admin.php?admin=menudetil">Cappuccino</a><li>
+					
+						
+					</ul>
+				</li>	
+				<li>
+					<a href="#">Laporan</a>
+					<ul>
+						<li><a href="admin.php?admin=laporanpesanan">Laporan Data Pesanan</a></li>
+						<li><a href="admin.php?admin=laporanpendapatan">Laporan Data Pendapatan</a></li>
+						<li><a href="admin.php?admin=laporanpesananinvalid">Laporan Pesanan Tidak dibayar</a></li>
+					</ul>
+				</li>
+				<li><a href="admin.php?admin=inputmeja">Data Meja</a></li>
+				<li><a href="admin.php?admin=daftarkaryawan">Data Karyawan</a></li>
+				<li>
+					<a href="logout.php">Logout</a>
+				</li>	
+			</ul>
+			</div>
+		</div>
+		</div>
+		</div>
+		</div>
+			<div class="cleared reset-box"></div>
+			<div class="art-box art-sheet">
+					<div class="art-box-body art-sheet-body">
+					
+					<div class="main_content">
+	
+							<div class="panel_minuman">							
+								<div class="menu_content"></div>
+								<div class="judul_content"></div>
+								<div class="judul_content">
+									<ul>
+										<li><a>Menu Minuman</a></li>
+									</ul>
+								</div>
+							</div>
+							
+							<div class="panel_makanan">
+								<div class="menu_content2"></div>
+								<div class="judul_content2">
+									<ul>
+										<li><a>Menu Makanan</a></li>
+									</ul>
+								</div>
+							</div>
+						
+				</div>			
+				<div class="cleared"></div>
+						<div class="art-footer">
+							<div class="art-footer-body">
+
+										<div class="art-footer-text">
+											<p>Copyright &copy;  2015 SerbaBebas. All rights reserved.</p>
+																		</div>
+								<div class="cleared"></div>
+							</div>
+						</div>
+						<div class="cleared"></div>
+					</div>
+			</div>
+			<div class="cleared"></div>
+			<p class="art-page-footer">&nbsp;</p>
+			<div class="cleared"></div>
+	</div>
+</div>
+</body>
+</html>
